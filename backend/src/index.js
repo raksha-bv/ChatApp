b1f0f5c870ddac05e1c1b6f92eaa23fdfcd3b080
raksha-bv <rakshabv04@gmail.com>
2024-12-31 21:31:40 +0530

    complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,23 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.routes.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { connectDB } from "./lib/db.js";
 import cors from "cors";
-import { app, io, server } from "./lib/socket.js";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -16,16 +25,18 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-const port = process.env.PORT;
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-server.listen(port, () => {
-  console.log("Server listening at PORT:" + port);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
